55987f3cc6f6ba0c9221ed9260f399fe0583e62c
Blaz Vantur <blaz.vantur@gmail.com>
2024-09-12 08:05:23 +0200

    [2] try a different approach of having iOS classes in the project

---
+++ ShakeDetektorIOS/ShakeDetektorIOS.swift
@@ -0,0 +1,11 @@
+//
+//  ShakeDetektorIOS.swift
+//  ShakeDetektorIOS
+//
+//  Created by Blaz Vantur on 11. 09. 24.
+//
+
+import Foundation
+
+@objc public class ShakeDetektorIOS: NSObject {
+}
---
+++ inspektifyLib/src/nativeInterop/cinterop/ShakeDetektorIOS.h
@@ -0,0 +1,18 @@
+//
+//  ShakeDetektorIOS.h
+//  ShakeDetektorIOS
+//
+//  Created by Blaz Vantur on 11. 09. 24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShakeDetektorIOS.
+FOUNDATION_EXPORT double ShakeDetektorIOSVersionNumber;
+
+//! Project version string for ShakeDetektorIOS.
+FOUNDATION_EXPORT const unsigned char ShakeDetektorIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShakeDetektorIOS/PublicHeader.h>
+
+
