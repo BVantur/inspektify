5ce3427f1a457882866c7663b24fb941dd2b6d9e
Blaz Vantur <blaz.vantur@gmail.com>
2024-10-15 17:41:29 +0200

    [68] feat: format json for presentation

--- inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/data/KtorPayloadRepositoryImpl.kt
+++ inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/data/KtorPayloadRepositoryImpl.kt
@@ -1,15 +1,19 @@
 package sp.bvantur.inspektify.ktor.payload.data
 
+import kotlinx.serialization.json.Json
 import sp.bvantur.inspektify.ktor.payload.data.datasource.KtorPayloadLocalDataSource
 import sp.bvantur.inspektify.ktor.payload.data.mapper.KtorPayloadDataMapper
 import sp.bvantur.inspektify.ktor.payload.domain.KtorPayloadRepository
 import sp.bvantur.inspektify.ktor.payload.domain.model.KtorPayloadData
 
-internal class KtorPayloadRepositoryImpl(private val localDataSource: KtorPayloadLocalDataSource) :
-    KtorPayloadRepository {
+internal class KtorPayloadRepositoryImpl(
+    private val localDataSource: KtorPayloadLocalDataSource,
+    private val json: Json
+) : KtorPayloadRepository {
     override suspend fun getTransactionPayloadDetails(id: Long, isRequest: Boolean): KtorPayloadData =
         KtorPayloadDataMapper.toDomain(
             data = localDataSource.getTransactionOverviewDetails(id),
-            isRequest = isRequest
+            isRequest = isRequest,
+            json = json
         )
 }
--- inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/data/mapper/KtorPayloadDataMapper.kt
+++ inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/data/mapper/KtorPayloadDataMapper.kt
@@ -5,22 +5,24 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import sp.bvantur.inspektify.NetworkTrafficDataLocal
 import sp.bvantur.inspektify.ktor.payload.domain.model.KtorPayloadData
 
 internal object KtorPayloadDataMapper {
 
-    fun toDomain(data: NetworkTrafficDataLocal, isRequest: Boolean): KtorPayloadData = KtorPayloadData(
+    fun toDomain(data: NetworkTrafficDataLocal, isRequest: Boolean, json: Json): KtorPayloadData = KtorPayloadData(
         headers = if (isRequest) {
             formatHeadersAsAnnotatedString(data.requestHeaders)
         } else {
             formatHeadersAsAnnotatedString(data.responseHeaders)
         },
         payload = if (isRequest) {
-            data.requestPayload
+            prettyPrintJson(json, data.requestPayload)
         } else {
-            data.responsePayload
-        } ?: ""
+            prettyPrintJson(json, data.responsePayload)
+        }
     )
 
     private fun formatHeadersAsAnnotatedString(headers: Set<Map.Entry<String, List<String>>>?): AnnotatedString {
@@ -36,6 +38,17 @@ internal object KtorPayloadDataMapper {
                 append(headerValue.joinToString())
                 append("\n")
             }
+        }
+    }
+
+    private fun prettyPrintJson(json: Json, jsonString: String?): String {
+        jsonString ?: return ""
+
+        try {
+            val jsonElement = Json.parseToJsonElement(jsonString)
+            return json.encodeToString(JsonElement.serializer(), jsonElement)
+        } catch (ignore: Exception) {
+            return jsonString
         }
     }
 }
