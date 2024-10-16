d1f3566ea3d110ef4511d5b4157ee45fbd26f18b
BVantur <blaz.vantur@gmail.com>
2024-10-16 17:04:26 +0200

    [68] Format json for presentation (#71)

--- inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/di/KtorPayloadModule.kt
+++ inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/di/KtorPayloadModule.kt
@@ -1,6 +1,8 @@
 package sp.bvantur.inspektify.ktor.payload.di
 
+import kotlinx.serialization.json.Json
 import sp.bvantur.inspektify.ktor.core.di.AppComponents
+import sp.bvantur.inspektify.ktor.core.di.AppComponents.getAppModule
 import sp.bvantur.inspektify.ktor.payload.data.KtorPayloadRepositoryImpl
 import sp.bvantur.inspektify.ktor.payload.data.datasource.KtorPayloadLocalDataSource
 import sp.bvantur.inspektify.ktor.payload.domain.KtorPayloadRepository
@@ -10,10 +12,11 @@ internal object KtorPayloadModule {
     private var localDataSource: KtorPayloadLocalDataSource? = null
 
     fun getKtorPayloadRepository(
+        json: Json = getAppModule().json,
         localDataSource: KtorPayloadLocalDataSource = getKtorPayloadLocalDataSource()
     ): KtorPayloadRepository {
         if (repository == null) {
-            repository = KtorPayloadRepositoryImpl(localDataSource)
+            repository = KtorPayloadRepositoryImpl(localDataSource, json)
         }
         return repository!!
     }
