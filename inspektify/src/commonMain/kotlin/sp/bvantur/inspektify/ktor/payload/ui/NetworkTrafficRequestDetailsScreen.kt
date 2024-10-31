e32f4c3d522299559b1f9e784692c94a482c3952
Blaz Vantur <blaz.vantur@gmail.com>
2024-10-31 12:06:34 +0100

    feat: add selection on longpress inside of transaction details screen

--- inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/ui/NetworkTrafficRequestDetailsScreen.kt
+++ inspektify/src/commonMain/kotlin/sp/bvantur/inspektify/ktor/payload/ui/NetworkTrafficRequestDetailsScreen.kt
@@ -1,9 +1,11 @@
 package sp.bvantur.inspektify.ktor.payload.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,14 +52,16 @@ private fun NetworkTrafficPayloadDetailsScreen(
     viewState: NetworkTrafficPayloadDetailsViewState,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.fillMaxWidth().verticalScroll(rememberScrollState()).padding(8.dp)
-    ) {
-        viewState.headers?.let { headers ->
-            Text(headers)
-        }
-        viewState.payload?.let { body ->
-            Text(body)
+    Box(modifier = modifier.fillMaxWidth()) {
+        SelectionContainer(modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState()).padding(8.dp)) {
+            Column {
+                viewState.headers?.let { headers ->
+                    Text(headers)
+                }
+                viewState.payload?.let { body ->
+                    Text(body)
+                }
+            }
         }
     }
 }
