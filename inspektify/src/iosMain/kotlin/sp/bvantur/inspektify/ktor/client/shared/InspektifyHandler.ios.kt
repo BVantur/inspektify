1fc18bdafd6bbdb1315bfc505cd63d1d06a17e19
Blaz Vantur <blaz.vantur@gmail.com>
2024-11-03 12:06:10 +0100

    fix: prevent duplication of iOS quick actions on Home screen

--- inspektify/src/iosMain/kotlin/sp/bvantur/inspektify/ktor/client/shared/InspektifyHandler.ios.kt
+++ inspektify/src/iosMain/kotlin/sp/bvantur/inspektify/ktor/client/shared/InspektifyHandler.ios.kt
@@ -94,15 +94,20 @@ private val UIApplication.topWindow: UIWindow?
     }
 
 private fun setupQuickAction() {
+    if (UIApplication.sharedApplication.shortcutItems?.any {
+            (it as? UIApplicationShortcutItem)?.type == INSPEKTIFY_SHORTCUT_ITEM_TYPE
+        } == true) {
+        return
+    }
     UIApplication.sharedApplication.shortcutItems =
         (UIApplication.sharedApplication.shortcutItems?.toMutableList() ?: mutableListOf()) +
-        UIApplicationShortcutItem(
-            INSPEKTIFY_SHORTCUT_ITEM_TYPE,
-            INSPEKTIFY_SHORTCUT_ITEM_SHORT_NAME,
-            INSPEKTIFY_SHORTCUT_ITEM_LONG_NAME,
-            UIApplicationShortcutIcon.iconWithType(
-                UIApplicationShortcutIconType.UIApplicationShortcutIconTypeSearch
-            ),
-            mapOf<Any?, Any>()
-        )
+                UIApplicationShortcutItem(
+                    INSPEKTIFY_SHORTCUT_ITEM_TYPE,
+                    INSPEKTIFY_SHORTCUT_ITEM_SHORT_NAME,
+                    INSPEKTIFY_SHORTCUT_ITEM_LONG_NAME,
+                    UIApplicationShortcutIcon.iconWithType(
+                        UIApplicationShortcutIconType.UIApplicationShortcutIconTypeSearch
+                    ),
+                    mapOf<Any?, Any>()
+                )
 }
